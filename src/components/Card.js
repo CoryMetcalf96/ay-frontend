5a1bff4266346ca6026e3787562368a4bcd0d7f8
Cory Metcalf <corymetcalf@Corys-Macbook-Pro.local>
2022-06-16 11:04:26 -0400

    Added component templates and base styling

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+function Card (props) {
+    return (
+        <div className="card">
+            {/* Placeholder for person's image */}
+            <img className="home-pfp" src="https://www.nicepng.com/png/full/289-2892199_admin-09-17-facebook-no-profile-pic-female.png" alt="Anonymous profile picture" />
+            {/* Placeholder for person's name */}
+            <a href="#"><h5>First Last</h5></a>
+        </div>
+    )
+}
+
+export default Card;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,1 +1,11 @@
-fffggg
+function Navigation (props) {
+    return (
+        <div className="navigation">
+            <a className="home-btn" href="/">Home</a>
+            <a className="add-new-btn" href="/new">Add New</a>
+            <a className="search-btn" href="/search">Search</a>
+        </div>
+    )
+};
+
+export default Navigation;
