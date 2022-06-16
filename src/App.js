2cf51877e635557d1d5a267e0fa03889c2edd061
Jon Michael Uy <uy.jonmichael@gmail.com>
2022-06-16 12:13:30 -0400

    add a NEW page, add Main index button in Nav bar, add edit and delete buttons in show page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 /////// Import Components ///////
-import Header from './components/Header';
-import Navigation from './components/Navigation';
-import Card from './components/Card';
-import Footer from './components/Footer';
-import { Route, Switch } from 'react-router-dom';
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import { Route, Switch } from "react-router-dom";
 
 /////// Import Component Styling ///////
-import './style/app.css'
-import './style/card.css';
-import './style/footer.css';
-import './style/header.css';
-import './style/navigation.css';
+import "./style/app.css";
+import "./style/card.css";
+import "./style/footer.css";
+import "./style/header.css";
+import "./style/navigation.css";
 
 //Import Pages
-import Landing from './pages/Landing';
-import MainIndex from './pages/MainIndex';
-import Show from './pages/Show';
+import Landing from "./pages/Landing";
+import MainIndex from "./pages/MainIndex";
+import Show from "./pages/Show";
+import New from "./pages/New";
 
 function App() {
   return (
@@ -23,16 +24,19 @@ function App() {
       <Header />
       <Navigation />
       {/* filler for now */}
-      <Route exact path="/"> 
+      <Route exact path="/">
         <Landing />
       </Route>
-      <Route path="/mainindex"> 
+      <Route path="/mainindex">
         <MainIndex />
       </Route>
-      <Route exact path="/show"> 
+      <Route path="/show">
         <Show />
       </Route>
-     
+      <Route path="/new">
+        <New />
+      </Route>
+
       <Footer />
     </div>
   );
