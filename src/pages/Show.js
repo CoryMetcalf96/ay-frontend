39c17b9038dfb478772c4e838912508adaacb058
Jon Michael Uy <uy.jonmichael@gmail.com>
2022-06-17 13:39:59 -0400

    update route to Show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,7 +1,19 @@
+// import { useState, useEffect } from 'react'
+// import { Route, Switch } from 'react-router-dom'
+// import MainIndex from './MainIndex';
+
 function Show(props) {
+const id = props.match.params.id
+const people = props.people
+const person = people.find(p => p._id === id)
+
   return (
-    <div>
-      <h1> This is the Show page </h1>
+    <div className="person">
+      <h1> {person.name} </h1>
+      <img src={person.picture} alt={person.name} />
+      <h3>{person.quote}</h3>
+      <h3>{person.nation}</h3>
+      <h3>{person.location}</h3>
       <button>Edit Details</button>
       <button>Delete Person</button>
     </div>
