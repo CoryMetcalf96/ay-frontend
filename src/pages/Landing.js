57e63ff5823e6430f43f8eff400808355131ff44
Cory Metcalf <corymetcalf@Corys-Macbook-Pro.local>
2022-06-22 12:15:30 -0400

    Added dynamic backgrounds to explore page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,7 +2,7 @@ function Landing(props) {
   return (<div className="main-landing">
     <div className="landing">
   <h2 className="landing-text">SEIR Avatar</h2>
-  <h3 className="landing-text">April 26 - July 20, 2022 Cohort</h3>
+  <h3 className="landing-text">April 25 - July 20, 2022 Cohort</h3>
   <h4 className="landing-quote">“Sometimes life is like this tunnel. You can’t always see the light at the end of the tunnel, but if you keep moving, you will come to a better place.” —Iroh, Episode 2.20 “The Crossroads of Destiny”</h4>
   </div>
   <div className="landing-welcome">
--- src/pages/MainIndex.js
+++ src/pages/MainIndex.js
@@ -8,21 +8,100 @@ function MainIndex(props) {
       <div className="cards">
         {props.people.map((person) => (
           <div key={person._id}>
-            <div className="card">
-              <Link to={`/show/${person._id}`}>
+            {/* Check to see if the person is air nation to apply styling */}
+            {(person.nation == "Air") || (person.nation == "Default") ?
+              <div className="air-card">
+                <Link to={`/show/${person._id}`}>
+                  {
+                    person.picture
+                      ? <img className="mainindex-pfp" src={person.picture} alt={person.name} />
+                      : <img className="mainindex-pfp" src={anonymousPicture} alt="Anonymous Photo" />
+                  }
+                  <h2 className="mainindex-name">{person.name}</h2>
+                </Link>
                 {
-                  person.picture
-                    ? <img className="mainindex-pfp" src={person.picture} alt={person.name} />
-                    : <img className="mainindex-pfp" src={anonymousPicture} alt="Anonymous Photo" />
+                  person.quote
+                    ? <h3 className="mainindex-quote">"{person.quote}"</h3>
+                    : null
                 }
-                <h2 className="mainindex-name">{person.name}</h2>
-              </Link>
-              {
-                person.quote
-                  ? <h3 className="mainindex-quote">"{person.quote}"</h3>
-                  : null
-              }
-            </div>
+              </div>
+              : null}
+
+            {/* Check to see if the person is water nation to apply styling */}
+            {(person.nation == "Water") ?
+              <div className="water-card">
+                <Link to={`/show/${person._id}`}>
+                  {
+                    person.picture
+                      ? <img className="mainindex-water-pfp" src={person.picture} alt={person.name} />
+                      : <img className="mainindex-water-pfp" src={anonymousPicture} alt="Anonymous Photo" />
+                  }
+                  <h2 className="mainindex-name">{person.name}</h2>
+                </Link>
+                {
+                  person.quote
+                    ? <h3 className="mainindex-quote">"{person.quote}"</h3>
+                    : null
+                }
+              </div>
+              : null}
+
+            {/* Check to see if the person is fire nation to apply styling */}
+            {(person.nation == "Fire") ?
+              <div className="fire-card">
+                <Link to={`/show/${person._id}`}>
+                  {
+                    person.picture
+                      ? <img className="mainindex-pfp" src={person.picture} alt={person.name} />
+                      : <img className="mainindex-pfp" src={anonymousPicture} alt="Anonymous Photo" />
+                  }
+                  <h2 className="mainindex-name">{person.name}</h2>
+                </Link>
+                {
+                  person.quote
+                    ? <h3 className="mainindex-quote">"{person.quote}"</h3>
+                    : null
+                }
+              </div>
+              : null}
+                
+            {/* Check to see if the person is earth nation to apply styling */}
+            {(person.nation == "Earth") ?
+              <div className="earth-card">
+                <Link to={`/show/${person._id}`}>
+                  {
+                    person.picture
+                      ? <img className="mainindex-earth-pfp" src={person.picture} alt={person.name} />
+                      : <img className="mainindex-earth-pfp" src={anonymousPicture} alt="Anonymous Photo" />
+                  }
+                  <h2 className="mainindex-name">{person.name}</h2>
+                </Link>
+                {
+                  person.quote
+                    ? <h3 className="mainindex-quote">"{person.quote}"</h3>
+                    : null
+                }
+              </div>
+              : null}
+
+              {/* Check to see if the person is an instructor to apply styling */}
+            {(person.nation == "Instructor") ?
+              <div className="instructor-card">
+                <Link to={`/show/${person._id}`}>
+                  {
+                    person.picture
+                      ? <img className="mainindex-pfp" src={person.picture} alt={person.name} />
+                      : <img className="mainindex-pfp" src={anonymousPicture} alt="Anonymous Photo" />
+                  }
+                  <h2 className="mainindex-name">{person.name}</h2>
+                </Link>
+                {
+                  person.quote
+                    ? <h3 className="mainindex-quote">"{person.quote}"</h3>
+                    : null
+                }
+              </div>
+              : null}
           </div>
         ))
         }</div>);
